d43166a8ddce855784441bada1bed63b15126672
adityasarikar <adityasarikar@gmail.com>
2026-05-04 01:12:38 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,55 @@
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithPopup 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  updateDoc, 
+  doc, 
+  getDocs, 
+  query, 
+  where 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import { 
+  getStorage, 
+  ref, 
+  uploadBytes, 
+  getDownloadURL 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+
+// 🔑 Your Firebase config (replace this)
 const firebaseConfig = {
   apiKey: "YOUR_KEY",
   authDomain: "YOUR_PROJECT.firebaseapp.com",
   projectId: "YOUR_PROJECT",
   storageBucket: "YOUR_PROJECT.appspot.com",
-  appId: "XXXX"
+  appId: "YOUR_APP_ID"
 };
 
-firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+//Initialise Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+// Auth provider
+export const provider = new GoogleAuthProvider();
+export { signInWithPopup };
+
+// Firestore helpers
+export { collection, addDoc, updateDoc, doc, getDocs, query, where };
+
+// Storage helpers
+export { ref, uploadBytes, getDownloadURL };
